ef305b1ae391e9a61f9e98a7f1692aed5e5a8556
asharykin <aleksandr-sharykin@mail.ru>
2024-10-17 14:20:02 +0300

    added README.md and minor changes

--- routers/delivery.py
+++ routers/delivery.py
@@ -1,17 +1,20 @@
-from fastapi import APIRouter
-from entities import Delivery
-from services import DeliveryService
 from typing import List, Optional
+
+from fastapi import APIRouter, Depends
+
+from entities import Delivery
+from routers.auth import get_current_user
+from services import DeliveryService
 
 router = APIRouter()
 delivery_service = DeliveryService()
 
 
 @router.post("/deliveries", response_model=Delivery)
-def create_delivery(delivery: Delivery):
+def create_delivery(delivery: Delivery, current_user: str = Depends(get_current_user)):
     return delivery_service.save(delivery)
 
 
 @router.get("/deliveries", response_model=List[Delivery])
-def get_deliveries_by_sender_or_receiver(sender_id: Optional[int] = None, receiver_id: Optional[int] = None):
+def get_deliveries_by_sender_or_receiver(sender_id: Optional[int] = None, receiver_id: Optional[int] = None, current_user: str = Depends(get_current_user)):
     return delivery_service.find_all_or_by_sender_or_receiver_id(sender_id, receiver_id)
