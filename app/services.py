9e44aa86d3c5385116664fa76cf8530c82dac3a6
asharykin <aleksandr-sharykin@mail.ru>
2024-12-04 19:15:39 +0300

    Lab 5

--- app/services.py
+++ app/services.py
@@ -1,9 +1,11 @@
-from typing import Optional, List
+import json
+from typing import Optional
 
+import redis
 from passlib.context import CryptContext
+from pymongo import MongoClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from pymongo import MongoClient
 
 from entities import User, Delivery
 from models import Parcel
@@ -13,7 +15,10 @@ engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 MONGO_URL = "mongodb://mongo:27017/"
-client = MongoClient(MONGO_URL)
+mongo_client = MongoClient(MONGO_URL)
+
+REDIS_URL = "redis://redis:6379/deliveries"
+redis_client = redis.from_url(REDIS_URL, decode_responses=True)
 
 
 class UserService:
@@ -56,7 +61,7 @@ class UserService:
 
 class ParcelService:
     def __init__(self):
-        self.collection = client["mongo"]["parcels"]
+        self.collection = mongo_client["mongo"]["parcels"]
         self.collection.create_index([("user_id", 1)])
         self.save(Parcel(description="Clothes", weight=2.575, user_id=1, delivery_id=1))
         self.save(Parcel(description="Stationery", weight=0.425, user_id=2, delivery_id=2))
@@ -78,6 +83,7 @@ class ParcelService:
 class DeliveryService:
     def __init__(self):
         self.session = SessionLocal
+        self.redis_client = redis_client
 
     def save(self, delivery: Delivery):
         with self.session() as session:
@@ -88,10 +94,35 @@ class DeliveryService:
             return delivery
 
     def find_by_sender_or_receiver_id(self, sender_id: Optional[int] = None, receiver_id: Optional[int] = None):
-        with self.session() as session:
-            query = session.query(Delivery)
-            if sender_id:
-                query = query.filter(Delivery.sender_id == sender_id)
-            if receiver_id:
-                query = query.filter(Delivery.receiver_id == receiver_id)
-            return query.all()
+        deliveries = []
+
+        if sender_id:
+            keys = self.redis_client.keys(f"sender:{sender_id}:delivery:*")
+            for key in keys:
+                delivery_str = self.redis_client.get(key)
+                delivery_dict = json.loads(delivery_str)
+                deliveries.append(Delivery(**delivery_dict))
+
+        elif receiver_id:
+            keys = self.redis_client.keys(f"receiver:{receiver_id}:delivery:*")
+            for key in keys:
+                delivery_str = self.redis_client.get(key)
+                delivery_dict = json.loads(delivery_str)
+                deliveries.append(Delivery(**delivery_dict))
+
+        if not deliveries:
+            with self.session() as session:
+                query = session.query(Delivery)
+                if sender_id:
+                    query = query.filter(Delivery.sender_id == sender_id)
+                elif receiver_id:
+                    query = query.filter(Delivery.receiver_id == receiver_id)
+                deliveries = query.all()
+
+                for delivery in deliveries:
+                    delivery_dict = delivery.__dict__
+                    delivery_dict.pop("_sa_instance_state")
+                    self.redis_client.set(f"sender:{delivery.sender_id}:delivery:{delivery.id}", json.dumps(delivery_dict))
+                    self.redis_client.set(f"receiver:{delivery.receiver_id}:delivery:{delivery.id}", json.dumps(delivery_dict))
+
+        return deliveries
